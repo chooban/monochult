559808dd572601fd135340c37ca309717328c285
Ross Hendry <rhendry@googlemail.com>
2018-05-15 15:53:01 +0100

    Added linting

--- packages/www-chult/src/__tests__/index.test.js
+++ packages/www-chult/src/__tests__/index.test.js
@@ -1,19 +1,21 @@
-import { http } from '../index'
-import { stub, spy } from 'sinon'
+import { stub, spy } from 'sinon';
+import { http } from '../index';
 
-describe("Getting a day in chult", () => {
-  let status, send, res;
-  
+describe('Getting a day in chult', () => {
+  let status;
+  let send;
+  let res;
+
   beforeEach(() => {
-    status = stub()
-    send = spy()
-    res = { status, send }
-    status.returns(res)
-  })
- 
-  it("returns the day", () => {
-    http(null, res)
-    expect(status.calledWith(200)).toBeTruthy()
-    expect(send.calledOnce).toBeTruthy()
-  })
-})
+    status = stub();
+    send = spy();
+    res = { status, send };
+    status.returns(res);
+  });
+
+  it('returns the day', () => {
+    http(null, res);
+    expect(status.calledWith(200)).toBeTruthy();
+    expect(send.calledOnce).toBeTruthy();
+  });
+});
--- packages/www-chult/src/index.js
+++ packages/www-chult/src/index.js
@@ -1,6 +1,4 @@
-'use strict';
-
-import chult from "toa-chult";
+import chult from 'toa-chult';
 
 export function http(request, response) {
   response.status(200).send(chult.day());
